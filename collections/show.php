8be4f895dee1fb96e18b5365a248552330825443
Dave Widmer <dwidmer@bgsu.edu>
2013-02-01 13:42:10 -0500

    Adding collections/show.php. It is the new index.php for the enhanced collections plugin.

---
+++ collections/show.php
@@ -0,0 +1,32 @@
+<?php echo head(); ?>
+
+<div>
+	<p>Cool Vintage imag'ed..</p>
+
+	<p>Collection Text....</p>
+</div>
+
+<h2>Featured Paperbacks</h2>
+<div class="row gallery">
+<?php
+	foreach (get_random_featured_items(4) as $item):
+		$files = $item->getFiles();
+		$file = $files[0];
+
+		if ($file):
+?>
+	<div class="span3 item">
+		<?php if ($file->hasThumbnail()): ?>
+		<a href="<?php echo record_url($item, 'show'); ?>">
+			<img src="<?php echo $file->getWebPath('thumbnail'); ?>" alt="Thumbnail" class="shadow-box" />
+		</a>
+		<?php endif; ?>
+	</div>
+
+<?php 
+		endif;
+	endforeach;
+?>
+</div>
+
+<?php echo foot(); ?>
